75d4cbdd1b12a4dc368d1dbfdfde3fac97c03e5f
SteveOscar <steveoscaro@gmail.com>
2025-07-09 16:43:54 -0600

    begins debugging main game screen

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -94,280 +94,280 @@ const GameScreen = ({
   const [beenClicked, setBeenClicked] = useState([]);
   const [inPlay, setInPlay] = useState(false);
 
-  // Sound
-  const { playSound } = useSound();
+  // // Sound
+  // const { playSound } = useSound();
   
-  // Convenience sound functions
-  const playMoveSound = useCallback(() => playSound('whoosh'), [playSound]);
-  const playButtonSound = useCallback(() => playSound('tap'), [playSound]);
-  const playGameOverSound = useCallback(() => playSound('buzzer'), [playSound]);
-  const playVictorySound = useCallback(() => playSound('bell'), [playSound]);
+  // // Convenience sound functions
+  // const playMoveSound = useCallback(() => playSound('whoosh'), [playSound]);
+  // const playButtonSound = useCallback(() => playSound('tap'), [playSound]);
+  // const playGameOverSound = useCallback(() => playSound('buzzer'), [playSound]);
+  // const playVictorySound = useCallback(() => playSound('bell'), [playSound]);
 
-  // CardFlip refs for each tile
-  const cardRefs = useRef(Array(size[0] * size[1]).fill().map(() => React.createRef()));
+  // // CardFlip refs for each tile
+  // const cardRefs = useRef(Array(size[0] * size[1]).fill().map(() => React.createRef()));
 
-  // Show/hide tile helpers
-  const initialSingleTileShow = useCallback(
-    (id) => {
-      if (beenClicked.indexOf(id) !== -1) return;
-      if (cardRefs.current[id] && cardRefs.current[id].current) {
-        cardRefs.current[id].current.flip();
-      }
-      setNumbers((prev) => {
-        const next = [...prev];
-        next[id] = hiddenLetters[id];
-        return next;
-      });
-    },
-    [beenClicked, cardRefs, hiddenLetters]
-  );
+  // // Show/hide tile helpers
+  // const initialSingleTileShow = useCallback(
+  //   (id) => {
+  //     if (beenClicked.indexOf(id) !== -1) return;
+  //     if (cardRefs.current[id] && cardRefs.current[id].current) {
+  //       cardRefs.current[id].current.flip();
+  //     }
+  //     setNumbers((prev) => {
+  //       const next = [...prev];
+  //       next[id] = hiddenLetters[id];
+  //       return next;
+  //     });
+  //   },
+  //   [beenClicked, cardRefs, hiddenLetters]
+  // );
 
-  const tileHide = useCallback(
-    (id) => {
-      if (cardRefs.current[id] && cardRefs.current[id].current) {
-        cardRefs.current[id].current.flip();
-      }
-      setNumbers((prev) => {
-        const next = [...prev];
-        next[id] = '';
-        return next;
-      });
-    },
-    [cardRefs]
-  );
+  // const tileHide = useCallback(
+  //   (id) => {
+  //     if (cardRefs.current[id] && cardRefs.current[id].current) {
+  //       cardRefs.current[id].current.flip();
+  //     }
+  //     setNumbers((prev) => {
+  //       const next = [...prev];
+  //       next[id] = '';
+  //       return next;
+  //     });
+  //   },
+  //   [cardRefs]
+  // );
 
-  // Show/hide tiles in sequence
-  const showTiles = useCallback(
-    (shouldHide) => {
-      setTimeout(() => {
-        for (let i = 0; i < size[0]; i++) {
-          for (let j = 0; j < size[1]; j++) {
-            initialSingleTileShow(i * size[1] + j);
-          }
-        }
-      }, 500);
-      setTimeout(() => {
-        for (let i = size[0]; i < size[0] * 2; i++) {
-          for (let j = 0; j < size[1]; j++) {
-            initialSingleTileShow(i * size[1] + j);
-          }
-        }
-      }, 1200);
-      setTimeout(() => {
-        if (size[1] < 3) return;
-        for (let i = size[0] * 2; i < size[0] * 3; i++) {
-          for (let j = 0; j < size[1]; j++) {
-            initialSingleTileShow(i * size[1] + j);
-          }
-        }
-      }, 1700);
-      setTimeout(() => {
-        if (size[1] < 4) return;
-        for (let i = size[0] * 3; i < size[0] * 4; i++) {
-          for (let j = 0; j < size[1]; j++) {
-            initialSingleTileShow(i * size[1] + j);
-          }
-        }
-      }, 2200);
-      if (shouldHide) hideTiles();
-    },
-    [size, initialSingleTileShow]
-  );
+  // // Show/hide tiles in sequence
+  // const showTiles = useCallback(
+  //   (shouldHide) => {
+  //     setTimeout(() => {
+  //       for (let i = 0; i < size[0]; i++) {
+  //         for (let j = 0; j < size[1]; j++) {
+  //           initialSingleTileShow(i * size[1] + j);
+  //         }
+  //       }
+  //     }, 500);
+  //     setTimeout(() => {
+  //       for (let i = size[0]; i < size[0] * 2; i++) {
+  //         for (let j = 0; j < size[1]; j++) {
+  //           initialSingleTileShow(i * size[1] + j);
+  //         }
+  //       }
+  //     }, 1200);
+  //     setTimeout(() => {
+  //       if (size[1] < 3) return;
+  //       for (let i = size[0] * 2; i < size[0] * 3; i++) {
+  //         for (let j = 0; j < size[1]; j++) {
+  //           initialSingleTileShow(i * size[1] + j);
+  //         }
+  //       }
+  //     }, 1700);
+  //     setTimeout(() => {
+  //       if (size[1] < 4) return;
+  //       for (let i = size[0] * 3; i < size[0] * 4; i++) {
+  //         for (let j = 0; j < size[1]; j++) {
+  //           initialSingleTileShow(i * size[1] + j);
+  //         }
+  //       }
+  //     }, 2200);
+  //     if (shouldHide) hideTiles();
+  //   },
+  //   [size, initialSingleTileShow]
+  // );
 
-  const hideTiles = useCallback(() => {
-    const difficultyFactor = timeAdjustment(difficulty) * 1.3;
-    setTimeout(() => {
-      Animated.timing(timerText, {
-        toValue: 1,
-        duration: 1000,
-        useNativeDriver: false,
-      }).start();
-      Animated.timing(timerHeight, {
-        toValue: height * 0.05,
-        duration: 1800,
-        useNativeDriver: false,
-      }).start();
-    }, 2500 * difficultyFactor);
-    setTimeout(() => {
-      for (let i = 0; i < size[0]; i++) {
-        for (let j = 0; j < size[1]; j++) {
-          tileHide(i * size[1] + j);
-        }
-      }
-    }, 2500 * difficultyFactor);
-    setTimeout(() => {
-      for (let i = size[0]; i < size[0] * 2; i++) {
-        for (let j = 0; j < size[1]; j++) {
-          tileHide(i * size[1] + j);
-        }
-      }
-    }, 2700 * difficultyFactor);
-    setTimeout(() => {
-      if (size[1] < 3) return;
-      for (let i = size[0] * 2; i < size[0] * 3; i++) {
-        for (let j = 0; j < size[1]; j++) {
-          tileHide(i * size[1] + j);
-        }
-      }
-    }, 2900 * difficultyFactor);
-    setTimeout(() => {
-      if (size[1] < 4) return;
-      for (let i = size[0] * 3; i < size[0] * 4; i++) {
-        for (let j = 0; j < size[1]; j++) {
-          tileHide(i * size[1] + j);
-        }
-      }
-    }, 3000 * difficultyFactor);
-    setTimeout(() => {
-      setInPlay(true);
-      startTimer();
-    }, 3500 * gameStartDelay(difficulty, difficultyFactor));
-  }, [difficulty, size, tileHide, timerHeight, timerText]);
+  // const hideTiles = useCallback(() => {
+  //   const difficultyFactor = timeAdjustment(difficulty) * 1.3;
+  //   setTimeout(() => {
+  //     Animated.timing(timerText, {
+  //       toValue: 1,
+  //       duration: 1000,
+  //       useNativeDriver: false,
+  //     }).start();
+  //     Animated.timing(timerHeight, {
+  //       toValue: height * 0.05,
+  //       duration: 1800,
+  //       useNativeDriver: false,
+  //     }).start();
+  //   }, 2500 * difficultyFactor);
+  //   setTimeout(() => {
+  //     for (let i = 0; i < size[0]; i++) {
+  //       for (let j = 0; j < size[1]; j++) {
+  //         tileHide(i * size[1] + j);
+  //       }
+  //     }
+  //   }, 2500 * difficultyFactor);
+  //   setTimeout(() => {
+  //     for (let i = size[0]; i < size[0] * 2; i++) {
+  //       for (let j = 0; j < size[1]; j++) {
+  //         tileHide(i * size[1] + j);
+  //       }
+  //     }
+  //   }, 2700 * difficultyFactor);
+  //   setTimeout(() => {
+  //     if (size[1] < 3) return;
+  //     for (let i = size[0] * 2; i < size[0] * 3; i++) {
+  //       for (let j = 0; j < size[1]; j++) {
+  //         tileHide(i * size[1] + j);
+  //       }
+  //     }
+  //   }, 2900 * difficultyFactor);
+  //   setTimeout(() => {
+  //     if (size[1] < 4) return;
+  //     for (let i = size[0] * 3; i < size[0] * 4; i++) {
+  //       for (let j = 0; j < size[1]; j++) {
+  //         tileHide(i * size[1] + j);
+  //       }
+  //     }
+  //   }, 3000 * difficultyFactor);
+  //   setTimeout(() => {
+  //     setInPlay(true);
+  //     startTimer();
+  //   }, 3500 * gameStartDelay(difficulty, difficultyFactor));
+  // }, [difficulty, size, tileHide, timerHeight, timerText]);
 
-  // Timer logic
-  const startTimer = useCallback(() => {
-    Animated.timing(timerText, {
-      toValue: 0,
-      duration: 1000,
-      useNativeDriver: false,
-    }).start();
-    const baseTime = 3500;
-    const difficultyFactor = timeAdjustment(difficulty) * 1.2;
-    const timerDuration = baseTime * difficultyFactor * levelTimeAdjustment(baseTime, level);
-    Animated.timing(progress, {
-      toValue: 0,
-      duration: timerDuration,
-      useNativeDriver: false,
-    }).start();
-    setTimeout(() => {
-      handleTimerEnd();
-    }, timerDuration);
-  }, [difficulty, level, progress, timerText]);
+  // // Timer logic
+  // const startTimer = useCallback(() => {
+  //   Animated.timing(timerText, {
+  //     toValue: 0,
+  //     duration: 1000,
+  //     useNativeDriver: false,
+  //   }).start();
+  //   const baseTime = 3500;
+  //   const difficultyFactor = timeAdjustment(difficulty) * 1.2;
+  //   const timerDuration = baseTime * difficultyFactor * levelTimeAdjustment(baseTime, level);
+  //   Animated.timing(progress, {
+  //     toValue: 0,
+  //     duration: timerDuration,
+  //     useNativeDriver: false,
+  //   }).start();
+  //   setTimeout(() => {
+  //     handleTimerEnd();
+  //   }, timerDuration);
+  // }, [difficulty, level, progress, timerText]);
 
-  // On mount: fade in and show tiles
-  useEffect(() => {
-    Animated.timing(fadeAnim, {
-      toValue: 1,
-      duration: 1300,
-      useNativeDriver: false,
-    }).start();
-    showTiles(true);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  // // On mount: fade in and show tiles
+  // useEffect(() => {
+  //   Animated.timing(fadeAnim, {
+  //     toValue: 1,
+  //     duration: 1300,
+  //     useNativeDriver: false,
+  //   }).start();
+  //   showTiles(true);
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
-  // Tile click logic
-  const alreadyClicked = useCallback(
-    (id) => {
-      if (beenClicked.indexOf(id) !== -1) return true;
-      setBeenClicked((prev) => [...prev, id]);
-      return false;
-    },
-    [beenClicked]
-  );
+  // // Tile click logic
+  // const alreadyClicked = useCallback(
+  //   (id) => {
+  //     if (beenClicked.indexOf(id) !== -1) return true;
+  //     setBeenClicked((prev) => [...prev, id]);
+  //     return false;
+  //   },
+  //   [beenClicked]
+  // );
 
-  const clickTile = useCallback(
-    (id) => {
-      if (!inPlay) return;
-      if (alreadyClicked(id)) return;
-      playButtonSound();
-      setTimeout(() => {
-        setNumbers((prev) => {
-          const next = [...prev];
-          next[id] = hiddenLetters[id];
-          return next;
-        });
-        checkSelection(id);
-      }, 200);
-      const tilt = boardTilt[id];
-      tilt.setValue(1);
-      Animated.timing(tilt, {
-        toValue: 2,
-        duration: 300,
-        easing: Easing.spring,
-        useNativeDriver: false,
-      }).start();
-    },
-    [inPlay, alreadyClicked, playButtonSound, hiddenLetters, boardTilt]
-  );
+  // const clickTile = useCallback(
+  //   (id) => {
+  //     if (!inPlay) return;
+  //     if (alreadyClicked(id)) return;
+  //     playButtonSound();
+  //     setTimeout(() => {
+  //       setNumbers((prev) => {
+  //         const next = [...prev];
+  //         next[id] = hiddenLetters[id];
+  //         return next;
+  //       });
+  //       checkSelection(id);
+  //     }, 200);
+  //     const tilt = boardTilt[id];
+  //     tilt.setValue(1);
+  //     Animated.timing(tilt, {
+  //       toValue: 2,
+  //       duration: 300,
+  //       easing: Easing.spring,
+  //       useNativeDriver: false,
+  //     }).start();
+  //   },
+  //   [inPlay, alreadyClicked, playButtonSound, hiddenLetters, boardTilt]
+  // );
 
-  const checkSelection = useCallback(
-    (id) => {
-      const selected = numbers[id];
-      if (selected >= prevSelection) {
-        setPrevSelection(selected);
-        updateScore(numbers.filter((n) => n !== '').length);
-      } else {
-        playGameOverSound();
-        deliverVerdict(false);
-        setInPlay(false);
-        showTiles(false);
-        endGame();
-      }
-    },
-    [numbers, prevSelection, updateScore, deliverVerdict, endGame, playGameOverSound, showTiles]
-  );
+  // const checkSelection = useCallback(
+  //   (id) => {
+  //     const selected = numbers[id];
+  //     if (selected >= prevSelection) {
+  //       setPrevSelection(selected);
+  //       updateScore(numbers.filter((n) => n !== '').length);
+  //     } else {
+  //       playGameOverSound();
+  //       deliverVerdict(false);
+  //       setInPlay(false);
+  //       showTiles(false);
+  //       endGame();
+  //     }
+  //   },
+  //   [numbers, prevSelection, updateScore, deliverVerdict, endGame, playGameOverSound, showTiles]
+  // );
 
-  // Timer end
-  const handleTimerEnd = useCallback(() => {
-    const length = size[0] * size[1];
-    if (!inPlay || beenClicked.length === length) return;
-    playGameOverSound();
-    deliverVerdict(false);
-    setInPlay(false);
-    showTiles(false);
-    endGame();
-  }, [inPlay, beenClicked, size, playGameOverSound, deliverVerdict, showTiles, endGame]);
+  // // Timer end
+  // const handleTimerEnd = useCallback(() => {
+  //   const length = size[0] * size[1];
+  //   if (!inPlay || beenClicked.length === length) return;
+  //   playGameOverSound();
+  //   deliverVerdict(false);
+  //   setInPlay(false);
+  //   showTiles(false);
+  //   endGame();
+  // }, [inPlay, beenClicked, size, playGameOverSound, deliverVerdict, showTiles, endGame]);
 
-  // Render tiles
-  const renderTiles = () => {
-    let result = [];
-    for (let row = 0; row < size[1]; row++) {
-      for (let col = 0; col < size[0]; col++) {
-        let id = row * size[0] + col;
-        let letter = numbers[id];
-        result.push(
-          <View
-            key={id}
-            style={[
-              styles.tile,
-              {
-                left: col * CELL_SIZE + CELL_PADDING,
-                top: row * CELL_SIZE + CELL_PADDING,
-              },
-            ]}
-          >
-            <CardFlip
-              ref={cardRefs.current[id]}
-              style={styles.cardFlip}
-              duration={800}
-              flipDirection="y"
-              perspective={600}
-            >
-              <View style={styles.tileFace}>
-                {/* Hidden side (back) */}
-              </View>
-              <View
-                style={styles.tileFace}
-                onStartShouldSetResponder={() => {
-                  clickTile(id);
-                  return true;
-                }}
-              >
-                <Text allowFontScaling={false} style={styles.letter}>
-                  {letter}
-                </Text>
-              </View>
-            </CardFlip>
-          </View>
-        );
-      }
-    }
-    return result;
-  };
+  // // Render tiles
+  // const renderTiles = () => {
+  //   let result = [];
+  //   for (let row = 0; row < size[1]; row++) {
+  //     for (let col = 0; col < size[0]; col++) {
+  //       let id = row * size[0] + col;
+  //       let letter = numbers[id];
+  //       result.push(
+  //         <View
+  //           key={id}
+  //           style={[
+  //             styles.tile,
+  //             {
+  //               left: col * CELL_SIZE + CELL_PADDING,
+  //               top: row * CELL_SIZE + CELL_PADDING,
+  //             },
+  //           ]}
+  //         >
+  //           <CardFlip
+  //             ref={cardRefs.current[id]}
+  //             style={styles.cardFlip}
+  //             duration={800}
+  //             flipDirection="y"
+  //             perspective={600}
+  //           >
+  //             <View style={styles.tileFace}>
+  //               {/* Hidden side (back) */}
+  //             </View>
+  //             <View
+  //               style={styles.tileFace}
+  //               onStartShouldSetResponder={() => {
+  //                 clickTile(id);
+  //                 return true;
+  //               }}
+  //             >
+  //               <Text allowFontScaling={false} style={styles.letter}>
+  //                 {letter}
+  //               </Text>
+  //             </View>
+  //           </CardFlip>
+  //         </View>
+  //       );
+  //     }
+  //   }
+  //   return result;
+  // };
 
-  // Main render
-  const dimensionWidth = CELL_SIZE * size[0];
-  const dimensionHeight = CELL_SIZE * size[1];
+  // // Main render
+  // const dimensionWidth = CELL_SIZE * size[0];
+  // const dimensionHeight = CELL_SIZE * size[1];
   return (
     <View style={{ width: width, flex: 1 }}>
       <Image
@@ -380,7 +380,7 @@ const GameScreen = ({
         style={styles.backgroundTop}
         resizeMode="cover"
       />
-      <Animated.View style={{ opacity: fadeAnim }}>
+      {/* <Animated.View style={{ opacity: fadeAnim }}>
         <Animated.View
           style={[
             styles.timer,
@@ -397,8 +397,9 @@ const GameScreen = ({
           }}
         >
           {renderTiles()}
+          <Text>Hello</Text>
         </View>
-      </Animated.View>
+      </Animated.View> */}
     </View>
   );
 };
