c776d76351bb0aecba88681da07be8e8a0d5c267
SteveOscar <steveoscaro@gmail.com>
2025-07-12 14:53:50 -0600

    fixes tricky bugs in the tile flip animation logic

--- src/components/FlippableTile.js
+++ src/components/FlippableTile.js
@@ -1,50 +1,65 @@
+// src/components/FlippableTile.js
 import React, { useRef, useEffect } from 'react';
 import { Animated, StyleSheet, View } from 'react-native';
 
 const FlippableTile = ({ frontContent, backContent, isFlipped, duration = 500, style }) => {
-  const flipAnim = useRef(new Animated.Value(isFlipped ? 180 : 0)).current;
+const flipAnim = useRef(new Animated.Value(isFlipped ? 180 : 0)).current;
 
-  useEffect(() => {
-    Animated.timing(flipAnim, {
-      toValue: isFlipped ? 180 : 0,
-      duration,
-      useNativeDriver: true,
-    }).start();
-  }, [isFlipped, duration]);
+useEffect(() => {
+Animated.timing(flipAnim, {
+toValue: isFlipped ? 180 : 0,
+duration,
+useNativeDriver: true,
+}).start();
+}, [isFlipped, duration]);
 
-  const flipStyle = {
-    transform: [
-      { perspective: 1000 },
-      {
-        rotateY: flipAnim.interpolate({
-          inputRange: [0, 180],
-          outputRange: ['0deg', '180deg'],
-        }),
-      },
-    ],
-  };
+const frontStyle = {
+transform: [
+{
+rotateY: flipAnim.interpolate({
+inputRange: [0, 180],
+outputRange: ['0deg', '180deg'],
+}),
+},
+],
+};
 
-  return (
-    <Animated.View style={[styles.container, style, flipStyle]}>
-      <View style={styles.side}>{frontContent}</View>
-      <View style={[styles.side, styles.backSide]}>{backContent}</View>
-    </Animated.View>
-  );
+const backStyle = {
+transform: [
+{
+rotateY: flipAnim.interpolate({
+inputRange: [0, 180],
+outputRange: ['180deg', '360deg'],
+}),
+},
+],
+};
+
+return (
+<Animated.View style={[styles.container, style, { transform: [{ perspective: 1000 }] }]}>
+<Animated.View style={[styles.side, frontStyle]}>
+{frontContent}
+</Animated.View>
+<Animated.View style={[styles.side, styles.backSide, backStyle]}>
+{backContent}
+</Animated.View>
+</Animated.View>
+);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    position: 'relative',
-  },
-  side: {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    backfaceVisibility: 'hidden',
-  },
-  backSide: {
-    transform: [{ rotateY: '180deg' }],
-  },
+container: {
+position: 'relative',
+},
+side: {
+position: 'absolute',
+width: '100%',
+height: '100%',
+backfaceVisibility: 'hidden',
+},
+backSide: {
+// No additional transform here
+},
 });
 
 export default FlippableTile;
